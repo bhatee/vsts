cf2208e4a707e25bb0d7020620e163faf409718e
badgujar <badgujaramol@gmail.com>
2018-08-02 14:45:19 +0530

    first test vsts

---
+++ amol/src/main/java/amol/amol/App.java
@@ -0,0 +1,13 @@
+package amol.amol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
